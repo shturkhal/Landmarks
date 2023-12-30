cc81a6c8b87e53fd95511727d92108041628bda7
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-12-30 18:54:45 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Іван Штурхаль on 30.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
