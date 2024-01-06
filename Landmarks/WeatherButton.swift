2c7d54a157f57be97560a867e3556e861a93df94
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-01-06 17:59:24 +0200

    final

---
+++ Landmarks/WeatherButton.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButton.swift
+//  Landmarks
+//
+//  Created by Іван Штурхаль on 03.01.2024.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButton()
+}
